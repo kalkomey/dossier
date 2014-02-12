a59c5447558565299fa81154403de91ddf891edc
Joshua Kappers <josh@jkappers.me>
2014-02-12 16:17:36 -0600

    Add support for xlsx rendering using Axlsx gem

---
+++ lib/dossier/axlsx.rb
@@ -0,0 +1,16 @@
+require 'axlsx'
+
+module Dossier
+  class Axlsx
+    class_attribute :default_format     
+    self.default_format = :xlsx
+
+    def self.call(template)
+      "xlsx_package = ::Axlsx::Package.new(author: nil);" + 
+      template.source +
+      "; xlsx_package.to_stream.string;"
+    end
+  end
+end
+
+ActionView::Template.register_template_handler :axlsx, Dossier::Axlsx
